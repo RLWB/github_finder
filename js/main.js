38bce8bcc2520659e5e4da9d25718ff890c1860d
惠瑞 <rlwb1017669154@gmail.com>
2017-01-20 11:18:23 +0800

    Github Finder

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
     $.ajax({
       url:'https://api.github.com/users/'+username,
       data:{
-        client_id:'d9308aacf8b204d361fd',
-        client_secret:'62551cc02cee983fff0bac41baf170eb5a312c1c'
+        client_id:'c6b0deb5a7df8d9f9056',
+        client_secret:'8846ead77a333f71bfc92b63f1aee85cca145388'
       }
     }).done(function(user){
       $.ajax({
         url:'https://api.github.com/users/'+username+'/repos',
         data:{
-          client_id:'d9308aacf8b204d361fd',
-          client_secret:'62551cc02cee983fff0bac41baf170eb5a312c1c',
+          client_id:'c6b0deb5a7df8d9f9056',
+          client_secret:'8846ead77a333f71bfc92b63f1aee85cca145388',
           sort: 'created: asc',
           per_page: 5
         }
